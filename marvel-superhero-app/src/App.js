e570a9df425ee03e6251957711d60c92f2b5df87
Joann Cahill <cahilljm@aol.com>
2022-05-24 14:02:51 -0700

    Push setup to prevent api from going to github

--- marvel-superhero-app/src/App.js
+++ marvel-superhero-app/src/App.js
@@ -1,10 +1,51 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+import SuperheroApp from './components/AnimalApp';
+import SuperheroDetail from './components/AnimalDetails';
+import {Route, Routes, Link,  useParams} from 'react-router-dom';
+
+const {REACT_APP_API, REACH_APP_HASH} = process.env
 
 function App() {
-  return (
+
+  const Home = () => { 
+    
+    return (
+    <div className="container ">
+
+      <h1>Sea Creature Search</h1>
+
+      <SuperheroApp
+
+      />
+    </div>
+  )};
+
+  const SuperHeroDetailPage = () => { 
+    const {id} = useParams();
+    return (
+      <div detail-info>
+      <h1>Superhero Details</h1>
+      <ul>
+          <li className="link" ><Link to="/">Return Home</Link></li>
+        </ul>
+      <SuperheroDetail
+        id={id}
+      />
+      </div>
+   );}
+
+
+   return (
     <div className="App">
-     <h1>Hello</h1>
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route
+      path="/hero/:id"
+      element={<SuperHeroDetailPage/>}
+      />
+      </Routes>
     </div>
   );
 }
